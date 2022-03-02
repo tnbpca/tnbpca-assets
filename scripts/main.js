4a21f57139fb3910b830904fd3de1b502b526e93
tnbpca <user@example.com>
2022-03-02 20:05:24 +0000

    Add style/scripts

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	$("img").on("dragstart", function(a) {
+		a.preventDefault();
+	});
+	$(".popeyes").click(function (a) {
+		window.open("https://popeyeschicken.ca", '_blank');
+	});
+	$(".rexall").click(function (a) {
+		window.open("https://www.rexall.ca/", '_blank');
+	});
+	$(".rexall").click(function (a) {
+		window.open("https://www.rexall.ca/", '_blank');
+	});
+	$(".tinyhoppers").click(function (a) {
+		window.open("https://www.tinyhoppers.ca/", '_blank');
+	});
+	$(".willowbrae").click(function (a) {
+		window.open("https://willowbraechildcare.com/", '_blank');
+	});
+	$(".dairyqueen").click(function (a) {
+		window.open("https://www.dairyqueen.com/en-ca/", '_blank');
+	});
+	$(".hplanet").click(function (a) {
+		window.open("https://www.healthyplanetcanada.com/", '_blank');
+	});
+	$(".paramount").click(function (a) {
+		window.open("https://paramountfinefoods.com/", '_blank');
+	});
+});
